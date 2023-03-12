db6963386fd266d87f46559d1adf6f22bd618bf7
Leo Jiang <ljiang@flexport.com>
2023-03-12 17:34:14 -0500

    The index of Range() and my calculation is off 4 characters. Likely due to whitespace

--- main.js
+++ main.js
@@ -1,65 +1,84 @@
-***********************************
-********** Code snippets **********
-***********************************
+const MAX_LETTERS_MISSING_SELECTED_SENTENCE = 5
 
-== spacebar highlights sentence ==
-function highlightSentences(bodyElement) {
-  // Get the inner text of the body element and split it into sentences
-  const sentences = bodyElement.innerText.split(/(?<=\.|\?|\!)\s/g);
-  
-  // Highlight the first sentence
-  let currentSentenceIndex = 0;
-  sentences[currentSentenceIndex] = `<mark>${sentences[currentSentenceIndex]}</mark>`;
-  bodyElement.innerText = sentences.join('');
-  
-  // Add an event listener for spacebar presses
-  document.addEventListener('keydown', function(event) {
-    if (event.code === 'Space') {
-      // Remove the highlight from the current sentence
-      sentences[currentSentenceIndex] = sentences[currentSentenceIndex].replace(/<\/?mark>/g, '');
-      
-      // Move to the next sentence
-      currentSentenceIndex = (currentSentenceIndex + 1) % sentences.length;
-      
-      // Highlight the next sentence
-      sentences[currentSentenceIndex] = `<mark>${sentences[currentSentenceIndex]}</mark>`;
-      bodyElement.innerHTML = sentences.join('');
-      
-      // Prevent the spacebar from scrolling the page
-      event.preventDefault();
-    }
-  });
+function sentenceObject(startOffset, endOffset, sentence) {
+  this.startOffset = startOffset
+  this.endOffset = endOffset
+  this.sentence = sentence
 }
 
-== spacebar moves selects next sentence ==
-function selectNextSentence() {
+function highlightSentence(currentDom) {
+  // case 1: initial load - nothing is selected
+  // case 2: end of search
+  const isTextSelected = window.getSelection().type !== "None"
+  if (currentDom == undefined && !isTextSelected) {
+    return
+  }
+
+  // case 3: moving to next paragraph
+
+  // case 4 - main loop 
+  toggleCurrentAndNextSentence()
+}
+
+// spacebar moves selects next sentence 
+function toggleCurrentAndNextSentence() {
   const currentSelection = window.getSelection();
   const currentRange = currentSelection.getRangeAt(0);
+
+  // Track the begining and end of each sentence in paragraph.
   const currentParagraph = currentRange.startContainer.parentNode;
-  
-  // find the current sentence by splitting the paragraph text at the period
-  const currentSentence = currentParagraph.innerText
-    .substring(0, currentRange.startOffset)
-    .split(/(?<=\.|\?|\!)\s/g)
-    .pop();
-  
-  // find the start and end offsets of the next sentence
-  const nextSentenceStart = currentRange.startOffset + currentSentence.length + 1;
-  const nextSentenceEnd = currentParagraph.textContent.indexOf('.', nextSentenceStart) + 1;
-  debugger;
+  const allSentences = currentParagraph.textContent.split(/(?<=\.|\?|\!)\s/g)
+  const allSentencesObjs = []
+  var startIndex = 0
+  for (let i = 0; i < allSentences.length; i++) {
+    var sentence = allSentences[i]
+    var endIndex = startIndex + sentence.length
+    allSentencesObjs.push(new sentenceObject(startIndex, endIndex, sentence))
+    startIndex += sentence.length
+  }
+
+  // Get current & next sentence details
+  const currSentenceObj = findCorrespondingSentence(allSentencesObjs, currentRange)
+  var nextSentence
+  // Case - Only fragment of sentence selected. Select the entire sentence.
+  if (currSentenceObj.sentence.length + MAX_LETTERS_MISSING_SELECTED_SENTENCE > currentRange.toString().trim()) {
+    nextSentence = currSentenceObj
+  } else { // Case - select next sentence 
+    nxtSentenceIdx = allSentencesObjs.indexOf(currSentenceObj) + 1
+    if (nxtSentenceIdx > allSentencesObjs.length) {
+      // TODO - go to next <p> tag
+      Console.log("Going to next paragraph still in-development")
+    }
+
+    nextSentence = allSentencesObjs[nxtSentenceIdx]
+  }
+
   // create a new range for the next sentence
   const nextRange = document.createRange();
-  nextRange.setStart(currentRange.startContainer, nextSentenceStart);
-  nextRange.setEnd(currentRange.startContainer, nextSentenceEnd);
-  debugger;
+  nextRange.setStart(currentRange.startContainer, nextSentence.startOffset);
+  nextRange.setEnd(currentRange.startContainer, nextSentence.endOffset);
+
   // select the new range
   currentSelection.removeAllRanges();
   currentSelection.addRange(nextRange);
 }
 
+function findCorrespondingSentence(allSentencesObjs, currentRange) {
+  const targetStartIndex = currentRange.startOffset
+  const targetEndIndex = currentRange.endOffset
+
+  for (let i = 0; i < allSentencesObjs.length; i++) {
+    sentenceObj = allSentencesObjs[i]
+    if (sentenceObj.startOffset <= targetStartIndex && sentenceObj.endOffset >= targetEndIndex) {
+      return sentenceObj
+    }
+  }
+  return undefined
+}
+
 document.addEventListener('keydown', function(event) {
   if (event.code === 'Space') {
-    event.preventDefault(); 
-    selectNextSentence();
+    event.preventDefault();
+    highlightSentence();
   }
 });
